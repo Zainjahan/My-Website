9ea6c7188a8ba3d76efaa64b1055727d71a81804
Zain Jahan <zainjahan64@gmail.com>
2022-10-12 22:54:56 +0500

    new work

---
+++ src/pages/CrouseSlidel.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+
+export default function CrouselSlide() {
+  return (
+    <div>
+        <Carousel slide={false}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+      
+    </div>
+  )
+}
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -19,6 +19,7 @@ import Paper from '@mui/material/Paper';
 
 import BootstrapCarousel from './BootstrapCarousel';
 import { Typography } from '@mui/material';
+import CrouselSlide from './CrouseSlidel';
 
 
 
@@ -51,114 +52,7 @@ export default function MainPage(props) {
         <h2>Our Featured Innovations</h2>
       </div>
       <div>
-         <Carousel autoPlay={false} >
-        <div>
-            <img alt="" src="https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60" />
-            <p className="legend">Legend 1</p>
-        </div>
-        <div>
-            <img alt="" src="https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60" />
-            <p className="legend">Legend 2</p>
-        </div>
-        <div>
-            <img alt="" src="https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60"/>
-            <p className="legend">Legend 3</p>
-        </div>
-       
-    </Carousel>
-
-        {/* <br />
-        <div>
-          <div class='container-fluid' >
-            <div className="row title" style={{ marginBottom: "20px" }} >
-              <div class="col-sm-12 btn btn-warning">
-                How To Use Bootstrap Carousel In ReactJS
-              </div>
-            </div>
-          </div>
-          <div className='container-fluid' >
-            <Carousel>
-              <Carousel.Item style={{ 'height': "300px" }} >
-                <img style={{ 'height': "300px" }}
-                  className="d-block w-100"
-                  src={'assets/img/img2.jpg'} />
-                <Carousel.Caption>
-                  <h3>First Demo </h3>
-                </Carousel.Caption>
-              </Carousel.Item  >
-              <Carousel.Item style={{ 'height': "300px" }}>
-                <img style={{ 'height': "300px" }}
-                  className="d-block w-100"
-                  src={'assets/img/img1.jpg'} />
-                <Carousel.Caption>
-                  <h3>Second Demo</h3>
-                </Carousel.Caption>
-              </Carousel.Item>
-              <Carousel.Item style={{ 'height': "300px" }}>
-                <img style={{ 'height': "300px" }}
-                  className="d-block w-100"
-                  src={'assets/img/img3.jpg'} />
-                <Carousel.Caption>
-                  <h3>Third Demo</h3>
-                </Carousel.Caption>
-              </Carousel.Item>
-            </Carousel>
-          </div>
-        </div> */}
-
-        {/* <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
-    </ol>
-
-    <div class="carousel-inner" role="listbox">
-
-      <div class="item active">
-        <img src="img_chania.jpg" alt="Chania" width="460" height="345"/>
-        <div class="carousel-caption">
-          <h3>Chania</h3>
-          <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="img_chania2.jpg" alt="Chania" width="460" height="345"/>
-        <div class="carousel-caption">
-          <h3>Chania</h3>
-          <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
-        </div>
-      </div>
-    
-      <div class="item">
-        <img src="img_flower.jpg" alt="Flower" width="460" height="345"/>
-        <div class="carousel-caption">
-          <h3>Flowers</h3>
-          <p>Beautiful flowers in Kolymbari, Crete.</p>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="img_flower2.jpg" alt="Flower" width="460" height="345"/>
-        <div class="carousel-caption">
-          <h3>Flowers</h3>
-          <p>Beautiful flowers in Kolymbari, Crete.</p>
-        </div>
-      </div>
-  
-    </div>
-
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div> */}
+      <CrouselSlide/>
       </div>
       <Box
       sx={{
